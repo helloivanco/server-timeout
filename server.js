c83f9dd82cf16519a3a7a760d84ffc5ad01e7823
Ivan <hellopaidco@gmail.com>
2023-03-19 22:22:32 +0000

    update app timeout

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
+const http = require('http');
 const dotenv = require('dotenv');
 
 const app = express();
-// Load the .env file
 dotenv.config();
 
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  // Get the timeout duration from the request's query parameter
-  // If not provided, use a default value of 1 seconds
   const timeoutDuration = parseInt(req.query.timeout) || 1000;
 
   setTimeout(() => {
@@ -21,6 +19,9 @@ app.get('/', (req, res) => {
   }, timeoutDuration);
 });
 
-app.listen(port, () => {
+const server = http.createServer(app);
+server.setTimeout(10 * 60 * 1000); // Set the server timeout to 10 minutes (600000 ms)
+
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
